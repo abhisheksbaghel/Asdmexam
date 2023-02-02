eb3372922cf0eaf2a1899058772404bc4c0d541b
Abhishek Singh Baghel <abhisheksinghbaghel8@gmail.com>
2023-02-02 15:06:18 +0530

    commit2

---
+++ code.java
@@ -0,0 +1,38 @@
+class Code {
+    public static void main(String args[]) {
+        // System.out.println("Hello");
+        // int n1 = 0;
+        // int n2 = 1;
+        // int n3;
+        // System.out.print(n1 + " " + n2);
+        // for (int i = 2; i < 20; i++) {
+        // n3 = n1 + n2;
+        // System.out.print(" " + n3);
+        // n1 = n2;
+        // n2 = n3;
+
+        System.out.println("Prime numbers");
+    
+    int i=0;
+        int n=0;
+        String prm = "";
+        for(i=1;i<=20;i++)
+        {
+            int count= 0;
+            for(n=i;n>=1;n--)
+            {
+                if(i%n==0)
+                {
+                    count= count+1;
+                }
+            }
+            if(count==2)
+            {
+                prm = prm + i + " " ;
+            }
+        }
+        System.out.println("Prime numbers from 1 to 20 are :");
+        System.out.println(prm);
+
+        }
+}
